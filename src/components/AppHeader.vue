65b7cd9d4ee60a16b3fc9274f2938d2dddc77711
Alex97410 <alexandre.alandissem@epitech.eu>
2023-01-04 10:22:45 +0100

    all

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,118 @@
+<script>
+import Button from 'primevue/button';
+
+export default {
+    name: 'AppHeader',
+    data: function () {
+        const grid = Array(9)
+        // for (let i = 0; i < array.length; i++) {
+
+        //     grid[i] = ""
+        // }
+        grid["bot-left"] = " "
+        grid["bot-middle"] = " "
+        grid["bot-right"] = " "
+        grid["middle-left"] = " "
+        grid["middle-middle"] = " "
+        grid["middle-right"] = " "
+        grid["top-left"] = " "
+        grid["top-middle"] = " "
+        grid["top-right"] = " "
+        return { winner: " ", score: 0, grid }
+    },
+
+    methods: {
+        winnerEvent(event) {
+            counter++
+            if (counter % 2 == 0) {
+                this.grid[event.target.id] = "x"
+            }
+            else {
+                this.grid[event.target.id] = "0"
+            }
+            if (
+                (this.grid["top-left"] === this.grid["top-middle"]    && this.grid["top-middle"]    === grid["top-right"] && this.grid["top-left"] != "") ||
+                (this.grid["top-left"] === this.grid["middle-middle"] && this.grid["middle-middle"] === grid["bot-right"] && this.grid["top-left"] != " ") ||
+                (this.grid["top-left"] === this.grid["middle-left"]   && this.grid["middle-left"]   === grid["bot-left"] && this.grid["top-left"] != " ") ||
+                //--
+                (this.grid["middle-middle"] === this.gird["middle-left"] && this.grid["middle-middle"] === grid["middle-right"] && this.grid["middle-middle"] != " ") ||
+                (this.grid["middle-middle"] === this.gird["middle-top"]  && this.grid["middle-middle"] === grid["middle-bot"] && this.grid["middle-middle"] != " ") ||
+                //--
+                (this.grid["bot-right"] === this.grid["bot-middle"]    && this.grid["bot-middle"]    === this.grid["bot-left"] && this.grid["bot-right"] != " ") ||
+                (this.grid["bot-left"]  === this.grid["middle-middle"] && this.grid["middle-middle"] === this.grid["top-right"] && this.grid["bot-right"] != " ") ||
+                (this.grid["bot-right"] === this.grid["middle-right"]  && this.grid["middle-right"]  === this.grid["top-right"] && this.grid["bot-right"] != " ")
+
+            ) {
+                this.winner = this.grid[event.target.id]
+                this.grid = null
+            }
+        }
+
+    }
+}
+
+</script>
+
+<template>
+
+    <header>
+
+        <div id="Jeu">
+            <div>
+                <button class="button" id="top-left"   v-on:@click="winnerEvent($event)"> {{ grid["top-left"] }} </button>
+                <button class="button" id="top-middle" v-on:@click="winnerEvent($event)"> {{ grid["top-middle"] }}</button>
+                <button class="button" id="top-right"  v-on:@click="winnerEvent($event)"> {{ grid["top-right"] }} </button>
+            </div>
+            <div>
+                <button class="button" id="middle-left"   v-on:@click="winnerEvent($event)">{{ grid["middle-left"] }}</button>
+                <button class="button" id="middle-middle" v-on:@click="winnerEvent($event)">{{ grid["middle-middle"] }}</button>
+                <button class="button" id="middle-right"  v-on:@click="winnerEvent($event)">{{ grid["middle-left"] }}</button>
+            </div>
+            <div>
+                <button class="button" id="bot-left"   v-on:@click="winnerEvent($event)">{{ grid["bot-left"] }}</button>
+                <button class="button" id="bot-middle" v-on:@click="winnerEvent($event)">{{ grid["bot-left"] }}</button>
+                <button class="button" id="bot-right"  v-on:@click="winnerEvent($event)">{{ grid["bot-left"] }}</button>
+            </div>
+            <img src="../assets/fond.jpg" class="img">
+        </div>
+    </header>
+</template>
+
+<style scoped>
+body {
+    background-color: LightSteelBlue;
+    text-align: center;
+}
+.button{
+    border: 1px solid rgba(0, 0, 0, 0.8);
+    font-size: 15px;
+    background-color: green;
+}
+
+.img {
+    width: 450px;
+    height: 250px;
+
+}
+
+#Jeu {
+    display: inline-block;
+    background-color: SteelBlue;
+    margin: 0 auto;
+    padding: 10px;
+}
+
+#Jeu div {
+    margin: 0 auto;
+    overflow: hidden;
+}
+
+#Jeu button {
+    width: 100px;
+    height: 100px;
+    font-weight: bold;
+    font-size: 50px;
+    margin: 5px;
+    float: left;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import './assets/main.css'
+import PrimeVue from "primevue/config"
+import Button from 'primevue/button';
+
+
+
+
+createApp(App).mount('#app')
